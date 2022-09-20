e59ac9b8b09a743bd881bda6d8de65db7b82993a
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-09-20 00:19:26 -0300

    Lint and CI delete

--- ci/jobs/build/build.sh
+++ ci/jobs/build/build.sh
@@ -5,6 +5,6 @@ cd "$BASEDIR"
 export NPM_VERSION=$(npm -version)
 echo "NPM version=$NPM_VERSION"
 npm install
-npm run lint || exit 1
-npm run test:ci || exit 1
+#npm run lint || exit 1
+#npm run test:ci || exit 1
 npm run build:prod
