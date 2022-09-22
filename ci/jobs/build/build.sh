bdac2423dc0e89b5861852973c3687ea9e08b8cc
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-09-22 13:45:09 -0300

    before_script added in build

--- ci/jobs/build/build.sh
+++ ci/jobs/build/build.sh
@@ -3,7 +3,6 @@ set -o errexit
 BASEDIR=$(dirname "$0")"/.."
 cd "$BASEDIR"
 export NPM_VERSION=$(npm -version)
-export CHROME_BIN= /usr/bin/google-chrome
 echo "NPM version=$NPM_VERSION"
 npm install
 npm run lint || exit 1
