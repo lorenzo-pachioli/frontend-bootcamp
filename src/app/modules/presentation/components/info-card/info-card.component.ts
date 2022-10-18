1a005ab7ce29bcc794a9267f0911bd0ca6133ba0
lpachioli <lorenzopachioli@gmail.com>
2022-10-18 20:12:00 -0300

    tg-74 Replace info-card component  by especific components

--- src/app/modules/presentation/components/info-card/info-card.component.ts
+++ src/app/modules/presentation/components/info-card/info-card.component.ts
@@ -1,9 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import { IProject } from 'src/app/modules/core/interfaces/projectInterface';
-import { IEpic } from 'src/app/modules/core/interfaces/epicInterface';
-import { IStory } from 'src/app/modules/core/interfaces/storyInterface';
-import { UserService } from 'src/app/modules/api-rest/services/user/user.service';
 
 
 @Component({
@@ -13,75 +9,12 @@ import { UserService } from 'src/app/modules/api-rest/services/user/user.service
 })
 export class InfoCardComponent implements OnInit {
 
-	item = {
-		name: '',
-		description: ''
-	};
 	loading = true;
-	@Input() project?: IProject;
-	@Input() epic?: IEpic;
-	@Input() story?: IStory;
+	@Input() item?: any;
 
-	constructor(public datepipe: DatePipe, private userService: UserService) { }
+	constructor(public datepipe: DatePipe) { }
 
 	ngOnInit(): void {
-		this.setItemValue(this.project);
-		this.setItemValue(this.epic);
-		this.setItemValue(this.story);
-	}
-
-	setItemValue(input: IProject | IEpic | IStory): void {
-		if (input) {
-			this.item.name = input.name
-			this.item.description = input.description
-		}
-	}
-
-	setMemberUsername(id: string): string {
-		const user = this.userService.getUserById(id);
-		if (user) {
-			return `-> ${user.username}`
-		}
-		return '';
-	}
-
-	setOwner(): string {
-		if (this.story && this.story.owner) {
-			return `${this.setMemberUsername(this.story.owner)}`
-		}
-	}
-
-	setAssignedTo(): Array<any> {
-		if (this.story && this.story.assignedTo.length > 0) {
-			return this.story.assignedTo;
-		}
-		return [];
-	}
-
-	setPoints(): string {
-		if (this.story && this.story.points) {
-			return `${this.story.points}`
-		}
-	}
-
-	setCreated(): string {
-		if (this.story && this.story.created) {
-			const date = this.datepipe.transform(this.story.created, 'dd/MM/yyyy');
-			return `${date}`;
-		}
-	}
-
-	setDue(): string {
-		if (this.story && this.story.due) {
-			const date = this.datepipe.transform(this.story.due, 'dd/MM/yyyy');
-			return `${date}`;
-		}
-	}
-
-	setStatus(): string {
-		if (this.story && this.story.status) {
-			return `${this.story.status}`
-		}
 	}
 
 }
