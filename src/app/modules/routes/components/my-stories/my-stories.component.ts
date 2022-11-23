1ed69526e8f56833de8eec642ce02c9dd7900dc6
lpachioli <lorenzopachioli@gmail.com>
2022-11-23 17:13:38 -0300

    go from My stories page to specific story page

--- src/app/modules/routes/components/my-stories/my-stories.component.ts
+++ src/app/modules/routes/components/my-stories/my-stories.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { StoriesService } from 'src/app/modules/api-rest/services/stories/stories.service';
+import { IUrl } from 'src/app/modules/core/interfaces/urlInterface';
+import { NavigationService } from 'src/app/modules/core/services/navigation/navigation.service';
 
 @Component({
 	selector: 'app-my-stories',
@@ -10,7 +13,21 @@ export class MyStoriesComponent implements OnInit {
 
 	stories = [];
 	loading = true;
-	constructor(public storiesList: StoriesService) {
+	url: IUrl = {
+		path: ''
+	};
+
+	constructor(
+		private navigation: NavigationService,
+		public storiesList: StoriesService,
+		private router: Router,
+	) {
+		this.navigation.url.subscribe(sub => {
+			this.url.path = sub.path
+			this.url.project = sub.project
+			this.url.epic = sub.epic
+			this.url.story = sub.story
+		});
 		this.storiesList.storiesList$.subscribe(data => {
 			if (data.length > 0) {
 				this.stories = data;
@@ -25,5 +42,9 @@ export class MyStoriesComponent implements OnInit {
 
 	OnDestroy(): void {
 		this.storiesList.storiesList$.unsubscribe();
+	}
+
+	setRoute(id: number): string {
+		return `/my-projects/${this.url.project && this.url.project.id}/${this.url.epic && this.url.epic.id}/${id}`;
 	}
 }
